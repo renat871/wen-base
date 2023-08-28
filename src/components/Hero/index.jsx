2ce5fe28fe9a75b2e88af0f0397b123392f59d05
Renat871 <rena2007at@gmail.com>
2023-08-28 09:29:21 +0200

    added typing animations

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,3 +1,5 @@
+import { TypeAnimation } from "react-type-animation";
+
 import "./style.scss";
 
 import preview from "../../img/hero.png";
@@ -7,7 +9,25 @@ const Hero = () => {
         <section className="hero">
             <div className="container">
                 <div className="hero__inner">
-                    <h1 className="hero__title">$wen pump?</h1>
+                    <TypeAnimation
+                        sequence={[
+                            "$WEN something?",
+                            10000,
+                            "$WEN $base?",
+                            10000,
+                            "$WEN binance?",
+                            10000,
+                            "$WEN moon?",
+                            10000,
+                            "$WEN shillers?",
+                            10000,
+                        ]}
+                        wrapper="h1"
+                        speed={10}
+                        repeat={Infinity}
+                        cursor={false}
+                        className="hero__title"
+                    />
                     <img src={preview} alt="preview" className="hero__img" />
                 </div>
             </div>
